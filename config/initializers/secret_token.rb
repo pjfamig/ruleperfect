213a3b7890eb00a2c175046af6beb24e54b2c647
Paul Famiglietti <p0seid0n_@hotmail.com>
2014-07-03 20:08:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ruleperfect::Application.config.secret_key_base = '46a92cc898b48412ba9ea048f97b0831686cd215e21a5aa4662ba1960fc1e692d621cfd4effd63c53171e6272e1837faf898c060e928c015152b35b293bc1b89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ruleperfect::Application.config.session_store :cookie_store, key: '_ruleperfect_session'
