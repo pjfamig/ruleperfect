412dead9d20b8c33eebba5f453100541e71f2630
Paul Famiglietti <p0seid0n_@hotmail.com>
2015-04-14 21:48:21 -0700

    Update static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Ruleperfect::Application.routes.draw do
-  get "static_pages/home"
   get "static_pages/about"
+  
   resources :rules
   resources :posts
   
-  root 'posts#new'
+  # root 'posts#new'
+  root 'static_pages#home'
   
   get   'posts/new/:topic',to: 'posts#new', as: :topic
   get   'rules/:subject',to: 'rules#index', as: :subject
